3cf833877a0b5b5dd65b17b4d9bf7c0c36953cca
Maxim Sergeev <sergeevsci@yandex.ru>
2026-06-06 20:35:01 +0300

    +findById UserController - поиск пользователя по его id

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.catsgram.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
@@ -13,6 +14,12 @@ import java.util.Collection;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public User findById(@PathVariable long userId) {
+        return userService.findUserById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id = " + userId + " не найден"));
+    }
 
     @GetMapping
     public Collection<User> findAll() {
