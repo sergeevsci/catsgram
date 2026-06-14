b3cdd5e8ad6543fe3e921d2960c1d74b5b37942d
Maxim Sergeev <sergeevsci@yandex.ru>
2026-06-14 13:23:29 +0300

    ErrorResponse to record-class.

--- src/main/java/ru/yandex/practicum/catsgram/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/ErrorResponse.java
@@ -1,14 +1,3 @@
 package ru.yandex.practicum.catsgram.model;
 
-import lombok.Getter;
-
-@Getter
-public class ErrorResponse {
-    private final String error;
-    private final String description;
-
-    public ErrorResponse(String error, String description) {
-        this.error = error;
-        this.description = description;
-    }
-}
+public record ErrorResponse(String error, String description) {}
